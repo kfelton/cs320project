c4fef32810be8d6600bea8153f174a3fc37e228e
kfelton <kfelton@ycp.edu>
2012-03-23 09:56:33 -0400

    fixed a syntax error

--- src/edu/ycp/cs320/project/APA.java
+++ src/edu/ycp/cs320/project/APA.java
@@ -2,7 +2,7 @@ package edu.ycp.cs320.project;
 
 public class APA {
 
-public APA(String lname,String fname,String pushdate,String booktitle, String city, String company,String date,String Article,String title,String periodic, int Volume, int Issue,int pages,String webtitle,String date,String url ){
+public APA(String lname,String fname,String pushdate,String booktitle, String city, String company,String date,String Article,String title,String periodic, int Volume, int Issue,int pages,String webtitle,String date2,String url ){
 	
 	
 }
